b1bcd309744b3d07c81d50b96cd823c6b3705379
Nestor Gonzalez <nesgonsua@gmail.com>
2024-05-26 20:10:25 +0100

    Inicio

---
+++ src/test/java/com/nestor/MascotasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
